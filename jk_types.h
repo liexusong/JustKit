da1bbd0002495c126fd864c5de399ee19b57fd79
liexusong <liexusong@qq.com>
2013-12-30 16:23:26 +0800

    Add jk_types.h file

---
+++ jk_types.h
@@ -0,0 +1,14 @@
+#ifndef __JK_TYPES_H
+#define __JK_TYPES_H
+
+typedef unsigned long long jk_uint64_t;
+typedef unsigned long jk_uint32_t;
+typedef unsigned short jk_uint16_t;
+typedef unsigned char jk_uint8_t;
+
+typedef long long jk_int64_t;
+typedef long jk_int32_t;
+typedef short jk_int16_t;
+typedef char jk_int8_t;
+
+#endif
